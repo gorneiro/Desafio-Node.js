a0fc325713394b9cd1423d5ab13769c73545d387
Mateus <mateus-junior@hotmail.com>
2021-09-29 20:02:12 -0300

    Criando Request e Response

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express') //declarei a variavel express, depois de ter usado o npm install express para baixar o pacote do mesmo, assim consigo usar apenas chamando a função.
-const app = express() //declarei a constante app para usar o express
-app.use(express.json())
+const app = express() //declarei a constante app para usar o express.
 
-app.get("/Client", function(req,res)) //get - Receber dados
-app, post() //post - Enviar dados
-app.put() //put - Atualizar dados
-app.delete() //delete - deletar um resource
-
-
-
-app.listen(8080, () => {
-  console.log('API em operação http://localhost:8080')
+app.get('/', function (req, res) {
+  res.send('Sejá bem vindo ao meu site')
 })
+
+app.get('/signup', function (req, res) {
+  res.send('Efetue o seu registro:')
+})
+
+app.get('/signin', function (req, res) {
+  res.send('Acesse o site:')
+})
+
+//app.get("/teste/:name:")
+
+app.get('/name/:email', function (req, res) {
+  res.send('<h1>Ola' + req.params.email + '</h1>')
+})
+
+app.listen(8084, function () {
+  console.log('Servidor Rodando na URL http://localhost:8084')
+}) //Fiz a ligação do meu servidor.

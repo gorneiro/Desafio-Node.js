eb245cc1807d0715da759c8b1d51d41b0dcd4a20
Mateus <mateus-junior@hotmail.com>
2021-09-29 15:52:04 -0300

    Atualização de HTTP

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express') //declarei a variavel express, depois de ter usado o npm install express para baixar o pacote do mesmo, assim consigo usar apenas chamando a função.
-
-const app = express() //declarei a constante app para usar
-
+const app = express() //declarei a constante app para usar o express
 app.use(express.json())
+
+app.get("/Client", function(req,res)) //get - Receber dados
+app, post() //post - Enviar dados
+app.put() //put - Atualizar dados
+app.delete() //delete - deletar um resource
+
+
 
 app.listen(8080, () => {
   console.log('API em operação http://localhost:8080')

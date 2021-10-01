8d0075afbe0aac10be8c38e613976ab08fda9ed7
Mateus <mateus-junior@hotmail.com>
2021-10-01 15:24:31 -0300

    3 rotas em funcionamento

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 Primeiro começei preparando o ambiente, instalando o node.js, npm,Express,gitHub, Eslint.
 Logo em seguida fui atras do que era uma API, REST e RESTful.
 Assim criei meu index.js e a primeira linha de comando digitada foi app.listen para inicializar o nosso servidor.
+O primeiro teste foi feito no browser
 
 ---
 
 Verbos HTTP
-get: Receber dados de um Resource/endpoint
+Get: Receber dados de um Resource/endpoint
 Post: Enviar dados ou informações para serem processados por um resource/endpoint
 
 ---
@@ -18,5 +19,7 @@ Codigos de resposta
 400: bad request
 404: not found
 =======
+
 # Desafio-Node.js
->>>>>>> 73ba1410cf739a81b264b7304fa636b05abdcb34
+
+> > > > > > > 73ba1410cf739a81b264b7304fa636b05abdcb34
--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const express = require('express') //declarei a variavel express, depois de ter usado o npm install express para baixar o pacote do mesmo, assim consigo usar apenas chamando a função.
 const app = express() //declarei a constante app para usar o express.
 const jwt = require('jsonwebtoken') // declarei a constante jwt para criar o web token
-const teste = 'teste321' // Constante para senha no JWT
+const teste = 'teste321' // declarei a constante teste para a segunda diretriz senha no JWT
+const dados = require('./dados.json') //Informações de usuarios
 
 app.use(express.json()) //informei para que a constante app use a função de leitura do json
 
 app.post('/signup', function (req, res) {
-  const { name, email, password, telephones, id } = req.body
+  const { name, email, password, telephone } = req.body
 
-  if ((!name, !email, !password, !telephones, !id))
-    return res.status(204).json()
+  if ((!name, !email, !password, !telephone)) return res.status(204).json()
 
-  res.json({ name, email, password, telephones, id })
+  res.json({ name, email, password, telephone })
 }) //Criação da rota signup, onde vai coletar as informações para cadastro de usuario.
 
 app.get('/search', function (req, res) {
-  res.json(/*banco de dados*/)
-}) //Consultar usuarios já criados
+  res.json(dados) /*banco de dados*/
+}) //Consultar usuarios já cadastrados.
 
 app.post('/signin', function (req, res) {
   if (
     req.body.email == 'mateus-junior@teste.com' &&
     req.body.password == 'asdw123'
   ) {
-    const token = jwt.sign({ id: 1 }, teste, { expiresIn: 500 })
+    const token = jwt.sign({ id: 1 }, teste, { expiresIn: 5000 }) //Declarei a constante token para gerar o JWT, ele precisa de 3 diretrizes
     return res.json({ token })
   }
 

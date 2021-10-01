a4f3717f662ad4ab0af1d8e318650eec07c4c0af
Mateus <mateus-junior@hotmail.com>
2021-09-30 21:03:08 -0300

    Adicionando o PWT

---
+++ README.md
@@ -0,0 +1,18 @@
+Primeiro começei preparando o ambiente, instalando o node.js, npm,Express,gitHub, Eslint.
+Logo em seguida fui atras do que era uma API, REST e RESTful.
+Assim criei meu index.js e a primeira linha de comando digitada foi app.listen para inicializar o nosso servidor.
+
+---
+
+Verbos HTTP
+get: Receber dados de um Resource/endpoint
+Post: Enviar dados ou informações para serem processados por um resource/endpoint
+
+---
+
+Codigos de resposta
+200:ok
+201:created
+204:não tem conteúdo put, post, delete
+400: bad request
+404: not found
--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const express = require('express') //declarei a variavel express, depois de ter usado o npm install express para baixar o pacote do mesmo, assim consigo usar apenas chamando a função.
 const app = express() //declarei a constante app para usar o express.
+const jwt = require('jsonwebtoken') // declarei a constante jwt para criar o web token
+const teste = 'teste321' // Constante para senha no JWT
 
-app.get('/', function (req, res) {
-  res.send('Sejá bem vindo ao meu site')
-})
+app.use(express.json()) //informei para que a constante app use a função de leitura do json
 
-app.get('/signup', function (req, res) {
-  res.send('Efetue o seu registro:')
-})
+app.post('/signup', function (req, res) {
+  const { name, email, password, telephones, id } = req.body
 
-app.get('/signin', function (req, res) {
-  res.send('Acesse o site:')
-})
+  if ((!name, !email, !password, !telephones, !id))
+    return res.status(204).json()
 
-//app.get("/teste/:name:")
+  res.json({ name, email, password, telephones, id })
+}) //Criação da rota signup, onde vai coletar as informações para cadastro de usuario.
 
-app.get('/name/:email', function (req, res) {
-  res.send('<h1>Ola' + req.params.email + '</h1>')
-})
+app.get('/search', function (req, res) {
+  res.json(/*banco de dados*/)
+}) //Consultar usuarios já criados
+
+app.post('/signin', function (req, res) {
+  if (
+    req.body.email == 'mateus-junior@teste.com' &&
+    req.body.password == 'asdw123'
+  ) {
+    const token = jwt.sign({ id: 1 }, teste, { expiresIn: 500 })
+    return res.json({ token })
+  }
+
+  res.status(401).end()
+}) //Criação da rota signin,logar e criar o JWT
 
 app.listen(8084, function () {
   console.log('Servidor Rodando na URL http://localhost:8084')
-}) //Fiz a ligação do meu servidor.
+}) //Fiz a ligação do meu servidor

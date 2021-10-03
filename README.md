01bec571f3c0e949f3af78001b670384dafddaf3
Mateus <mateus-junior@hotmail.com>
2021-10-03 09:45:16 -0300

    Finalizado

--- README.md
+++ README.md
@@ -1,26 +1,68 @@
+# Desafio API em Node.JS
+
+Criação de uma API para validação de usuario e senha.
+
 Primeiro começei preparando o ambiente, instalando o node.js, npm,Express,gitHub, Eslint.
 Logo em seguida fui atras do que era uma API, REST e RESTful.
 Assim criei meu index.js e a primeira linha de comando digitada foi app.listen para inicializar o nosso servidor.
-O primeiro teste foi feito no browser
+Logo em seguida começei a criar as rotas, com get e post.
+Para a consulta de dados eu criei o arquivo dados.json para armazenar as informações.
+Fiz a criação do JWT, com 3 parametros para retornar o 'token'.
+E então a criação do db.js minha maior dificuldade no projeto, foi a criação da logica e a sintaxe, precisei pesquisar muito para enteder.
+Depois de todos os ajustes, eu fiz o deploy no heroku.
 
+Abaixo segue algumas das fontes de pesquisa que eu estudei para a criação da aplicação!
+
+O que é API? REST e RESTful? | Mayk Brito
+https://www.youtube.com/watch?v=ghTrp1x_1As
+
+Autenticação JSON Web Token (JWT) em Node.js
+https://www.youtube.com/watch?v=D0gpL8-DVrc
+
+API com Node.js - Passo 12 - Login + JWT Token
+https://www.youtube.com/watch?v=A-c643zCW7E
+
+Curso de Node.js - Rotas #07
+https://www.youtube.com/watch?v=UMI7kFwmAHo&list=PLJ_KhUnlXUPtbtLwaxxUxHqvcNQndmI4B&index=9
+
+Curso de Node.js - Parâmetros #08
+https://www.youtube.com/watch?v=G9b-Zi0rg3o&list=PLJ_KhUnlXUPtbtLwaxxUxHqvcNQndmI4B&index=9
+
+Curso de Node.js - Nodemon #09
+https://www.youtube.com/watch?v=u3MrPxq_RyA&list=PLJ_KhUnlXUPtbtLwaxxUxHqvcNQndmI4B&index=10
+
+Curso de Node.js - Exibindo HTML #10
+https://www.youtube.com/watch?v=UkwLcuzJRDQ&list=PLJ_KhUnlXUPtbtLwaxxUxHqvcNQndmI4B&index=11
 
 Verbos HTTP
 Get: Receber dados de um Resource/endpoint
 Post: Enviar dados ou informações para serem processados por um resource/endpoint
+https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
 
+Regex
+https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+https://regexr.com/
+https://www.horadecodar.com.br/2020/09/07/expressao-regular-para-validar-e-mail-javascript-regex/
 
+Exportação de modulos
+https://www.sitepoint.com/understanding-module-exports-exports-node-js/
+
+Heroku
+https://dzone.com/articles/deploy-your-node-express-app-on-heroku-in-8-easy-s
+
+FS
+https://nodejs.org/docs/v0.3.1/api/fs.html
+
+Node.js readFileSync e writeFileSync: ler e gravar arquivos com File System (CONCLUIR O TEXT ROBOT)
+https://www.youtube.com/watch?v=TZRMh4BZTS0
+
+Array.prototype.find()
+https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/find
 
 Codigos de resposta
+https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
 200:ok
 201:created
 204:não tem conteúdo put, post, delete
 400: bad request
 404: not found
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 9dff14dd559aef4edd287c9bfc77e76592d445e6
-
-# Desafio-Node.js
-
-> > > > > > > 73ba1410cf739a81b264b7304fa636b05abdcb34
--- index.js
+++ index.js
@@ -1,49 +1,50 @@
 const express = require('express') //declarei a variavel express, depois de ter usado o npm install express para baixar o pacote do mesmo, assim consigo usar apenas chamando a função.
 const app = express() //declarei a constante app para usar o express.
-const jwt = require('jsonwebtoken') // declarei a constante jwt para criar o web token
-const teste = 'teste321' // declarei a constante teste para a segunda diretriz senha no JWT
-//const dados = require('./dados.json') Informações de usuarios
-
-const validar = require('./validation.js')
-const fs = require('fs')
-const db = require('./db')
-const port = process.env.PORT || 3000
+const jwt = require('jsonwebtoken') //declarei a constante jwt para criar o web token
+const teste = 'teste321' //declarei a constante teste para a segunda diretriz senha no JWT
+const validar = require('./validation.js') //declarei função regex
+const fs = require('fs') //declarei a biblioteca fs
+const db = require('./db') //declarei o acesso ao db.js
+const port = process.env.PORT || 3000 //declarei a porta para usar no heroku
 
 app.use(express.json()) //informei para que a constante app use a função de leitura do json
 
+//Criação da rota signup, onde vai coletar as informações para cadastro de usuario.
 app.post('/signup', function (req, res) {
   const { name, email, password, telephone } = req.body
-
+  //Validar se os campos não estão em brancos
   if ((!name, !email, !password, !telephone))
     return res
       .status(422)
       .json({ message: 'Um ou mais campos necessarios estão vazios' })
-
+  //Validar se o e-mail é valido com uma função regex
   if (!validar.validaremail(email))
     return res.status(422).json({ message: 'Email invalido' })
-
+  //Onde vai ser adicionado campos novos, depois das validações.
   const newUser = {
     name,
     email,
     password,
     telephone
   }
-  // dados.users.push(newUser)
+  // Pegar as informaçõs dos campos acima e adicionar no dados.json
   db.writedbObject('users', newUser)
   res.json({ name, email, password, telephone })
-}) //Criação da rota signup, onde vai coletar as informações para cadastro de usuario.
+})
 
+//Consultar usuarios já cadastrados no banco de dados.
 app.get('/search', function (req, res) {
   const search = db.readdbObject('users')
-  res.json(search) /*banco de dados*/
-}) //Consultar usuarios já cadastrados.
+  res.json(search)
+})
 
+//Criação da rota signin,logar e criar o JWT
 app.post('/signin', function (req, res) {
   //Validar os dados da requisição
   const { email, password } = req.body
   if ((!email, !password))
     return res.status(422).json({ message: 'Email ou senha invalidas' })
-
+  //Validar se o e-mail é valido com uma função regex
   if (!validar.validaremail(email))
     return res.status(422).json({ message: 'Email invalido' })
   //Validar se a informação esta no nosso db.js
@@ -51,12 +52,11 @@ app.post('/signin', function (req, res) {
   //se não estiver, informar mensagem de erro.
   if (!recUsuario) return res.status(401).end()
   //se estiver, criar um token e retornar o mesmo.
-
   const token = jwt.sign(recUsuario, teste, {
     expiresIn: 5000
   }) //Declarei a constante token para gerar o JWT, ele precisa de 3 diretrizes
   return res.json({ token })
-}) //Criação da rota signin,logar e criar o JWT
+})
 
 app.listen(port, function () {
   console.log('Servidor Rodando na URL http://localhost:' + port)
